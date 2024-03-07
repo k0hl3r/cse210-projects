578e5fc0709c136610454dcab664be3007d8ba87
k0hl3r <pra23012@byui.edu>
2024-03-06 20:54:32 -0700

    done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Legal Assistant";
+        job1._company = "Arizona Consumer Law Group";
+        job1._startYear = 2023;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Graphic Designer";
+        job2._company = "Cerifi";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Kohler Pratt";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
